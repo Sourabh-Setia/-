b4dc328f2c2fe61f9cc302a969023e15ae1b8bbb
Sourabh Setia <sourabhsetia@192.168.1.5>
2025-03-30 12:27:29 +0530

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio/", // Change this to your GitHub repo name if deploying to GitHub Pages
   plugins: [react()],
 });
-
