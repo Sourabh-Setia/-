aca15caa2a7c82abf15e9d3d5e29f1052c3b748d
Sourabh-Setia <sourabhsetia0786@gmail.comm>
2025-03-30 14:55:39 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/", // Change this to your GitHub repo name if deploying to GitHub Pages
+  // base: "/portfolio/",
   plugins: [react()],
 });
